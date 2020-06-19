f8e808addc37a572e0decd1d724bf7824c7fcef9
Ramon Alejandrino Silva <ramon_al_sil@yahoo.com.ar>
2020-06-19 16:42:19 -0300

    Se modifico el body

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import log from '../statics/v5-new-logo.png'
 
 const Index = () => (<div className="container">
-  <h1 className="post-title">Titulo1</h1>
+  <h2 className="post-title">Titulo1</h2>
   <p>Auto-sizing The example below uses a flexbox utility to vertically center the contents and changes .col to .col-auto so that your columns only take up as much space as needed. Put another way, the column sizes itself based on the contents.</p>
+  <p> <a href="https://v5.getbootstrap.com"><img alt="New Bootstrap docs layout" src={log} /></a></p>
+  <p>Mas texto aqui</p>
 </div>)
 
 export default Index;
