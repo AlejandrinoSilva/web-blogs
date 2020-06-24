2baa390b3d269633143e89dd913334c9335863fe
Ramon Alejandrino Silva <ramon_al_sil@yahoo.com.ar>
2020-06-24 12:09:02 -0300

    Updates

--- precache-manifest.35cfe102d2a277fe812610a8fdedc7ba.js
+++ precache-manifest.35cfe102d2a277fe812610a8fdedc7ba.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ff37d2a26a45cc020ad5daa060235047",
+    "revision": "9d5cbb0780899deb9b99079c6f8b21f0",
     "url": "/web-blogs/index.html"
   },
   {
-    "revision": "779e7aa56c66ee3238ed",
+    "revision": "65e2df76879d5f6dad8a",
     "url": "/web-blogs/static/css/main.b2e95ff4.chunk.css"
   },
   {
-    "revision": "7db9ea38d8b85a11192b",
-    "url": "/web-blogs/static/js/2.e7c54b41.chunk.js"
+    "revision": "dd84c0b7350b51419612",
+    "url": "/web-blogs/static/js/2.dee00eee.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/web-blogs/static/js/2.e7c54b41.chunk.js.LICENSE.txt"
+    "url": "/web-blogs/static/js/2.dee00eee.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "779e7aa56c66ee3238ed",
-    "url": "/web-blogs/static/js/main.0dab9f7f.chunk.js"
+    "revision": "65e2df76879d5f6dad8a",
+    "url": "/web-blogs/static/js/main.73a7b17b.chunk.js"
   },
   {
     "revision": "263dabd8d6e602acdbbf",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/web-blogs/precache-manifest.2325d631318606c4b29db5855565cb03.js"
+  "/web-blogs/precache-manifest.35cfe102d2a277fe812610a8fdedc7ba.js"
 );
 
 self.addEventListener('message', (event) => {
