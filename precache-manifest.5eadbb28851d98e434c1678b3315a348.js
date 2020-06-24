d194c0fee8811dab39a2d2b4a53d114a2c8ed7de
Ramon Alejandrino Silva <ramon_al_sil@yahoo.com.ar>
2020-06-24 09:36:47 -0300

    Updates

---
+++ precache-manifest.5eadbb28851d98e434c1678b3315a348.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb1ea7ec4d2df4040e03f5eddfaf31e",
+    "url": "/web-blogs/index.html"
+  },
+  {
+    "revision": "f58ddf2c8dc73be6fe14",
+    "url": "/web-blogs/static/css/main.b2e95ff4.chunk.css"
+  },
+  {
+    "revision": "ffc7f44defb3a287e839",
+    "url": "/web-blogs/static/js/2.be1883f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/web-blogs/static/js/2.be1883f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f58ddf2c8dc73be6fe14",
+    "url": "/web-blogs/static/js/main.73a67f55.chunk.js"
+  },
+  {
+    "revision": "263dabd8d6e602acdbbf",
+    "url": "/web-blogs/static/js/runtime-main.3dc05373.js"
+  },
+  {
+    "revision": "53b9bcb3316c3cd8dbb72f21c961860a",
+    "url": "/web-blogs/static/media/v5-new-logo.53b9bcb3.png"
+  }
+]);
